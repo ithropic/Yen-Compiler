b8a8f8ebaada4c3923dba469821761048ce8b6c4
rayen <pr_ahmedbrahim@esi.dz>
2026-08-22 17:35:27 +0100

    renamed Scanner class to Lexer for consistency.

--- src/main/java/com/yen/compiler/Compiler.java
+++ src/main/java/com/yen/compiler/Compiler.java
@@ -8,43 +8,47 @@ import java.util.List;
 
 public class Compiler {
   static boolean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length != 1) // the program command "yen" is not counted as an argument
-                          // unlike C/C++ so args[0] will be [file].
+                          // unlike C/C++ so args[0] will be [file.yen].
     {
-     System.out.println("Usage: yen [file]");
-     System.exit(64);
-    }
-    else {
-     runFile(args[0]); 
+      System.out.println("Usage: yen [file.yen]");
+      System.exit(64);
+    } else {
+      runFile(args[0]);
     }
   }
 
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path)); // Paths.get() converts the String into a system path object
-                                                        // if it is not an absolute path the OS will look for the file 
+                                                        // if it is not an absolute path the OS will look for the file
                                                         // in the current working directory.
     run(new String(bytes, Charset.defaultCharset())); // UTF-8 by default.
 
-    if (hadError) System.err.println(65);
-    
+    if (hadError)
+      System.err.println(65);
+
   }
 
   private static void run(String source) {
-    Scanner scanner = new Scanner(source);
-    List<Token> tokens = scanner.scanTokens();
+    Lexer lexer = new Lexer(source);
+    List<Token> tokens = lexer.scanTokens();
     Parser parser = new Parser(tokens);
 
     List<Stmt> statements = parser.parse();
-    if (hadError) return;
+    if (hadError)
+      return;
 
     Resolver resolver = new Resolver();
     resolver.resolveProgram(statements);
-    if (hadError) return;
+    if (hadError)
+      return;
 
     TypeChecker typeChecker = new TypeChecker();
     typeChecker.check(statements);
-    if (hadError) return;
+    if (hadError)
+      return;
 
     BytecodeCompiler bytecode = new BytecodeCompiler();
     Chunk chunk = bytecode.compile(statements);
@@ -62,13 +66,12 @@ public class Compiler {
     hadError = true;
   }
 
-  static void error(Token token , String message) {
-      if (token.type ==  TokenType.EOF) {
-        report(token.line, " at end", message);
-      } else {
-        report(token.line, " at '" + token.lexeme + "'", message);
-      }
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
     }
-
+  }
 
 }
--- src/main/java/com/yen/compiler/Lexer.java
+++ src/main/java/com/yen/compiler/Lexer.java
@@ -7,13 +7,12 @@ import java.util.Map;
 
 import static com.yen.compiler.TokenType.*;
 
-class Scanner {
+class Lexer {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
   private int start = 0;
   private int current = 0;
   private int line = 1;
-
 
   private static final Map<String, TokenType> keywords;
 
@@ -36,15 +35,12 @@ class Scanner {
     keywords.put("void", VOID);
   }
 
-  Scanner(String source)
-  {
+  Lexer(String source) {
     this.source = source;
   }
 
-  List<Token> scanTokens() 
-  {
-    while (!isAtEnd())
-    {
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
       start = current; // pointing to the beginning of the next lexeme.
       scanToken();
     }
@@ -56,66 +52,87 @@ class Scanner {
   private void scanToken() {
     char c = advance();
     switch (c) {
-      case '(' : addToken(LEFT_PAREN); break;
-      case ')' : addToken(RIGHT_PAREN); break;
-      case '{' : addToken(LEFT_BRACE); break;
-      case '}' : addToken(RIGHT_BRACE); break;
-      case ',' : addToken(COMMA); break;
-      case '.' : addToken(DOT); break;
-      case ';' : addToken(SEMICOLON); break;
-      case '+' : addToken(PLUS); break;
-      case '-' : addToken(MINUS); break;
-      case '*' : addToken(STAR); break;
+      case '(':
+        addToken(LEFT_PAREN);
+        break;
+      case ')':
+        addToken(RIGHT_PAREN);
+        break;
+      case '{':
+        addToken(LEFT_BRACE);
+        break;
+      case '}':
+        addToken(RIGHT_BRACE);
+        break;
+      case ',':
+        addToken(COMMA);
+        break;
+      case '.':
+        addToken(DOT);
+        break;
+      case ';':
+        addToken(SEMICOLON);
+        break;
+      case '+':
+        addToken(PLUS);
+        break;
+      case '-':
+        addToken(MINUS);
+        break;
+      case '*':
+        addToken(STAR);
+        break;
 
-      case '!' : 
-                addToken(match('=')  ? BANG_EQUAL : BANG);
-                break;
-      case '=' :
-                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
-                break;
-      case '<' : 
-                addToken(match('=') ? LESS_EQUAL : LESS);
-                break;
-      case '>' : 
-                addToken(match('=') ? GREATER_EQUAL :  GREATER);
-                break;
-      case '/' :
-                if (match('/')) {
-                  while(peek() != '\n' && !isAtEnd()) advance(); // here  advance will be returning each char it 
-                                                                  // encounters, but java will simply ignore them
-                                                                  // since they aren't assingned to anything.
-                } 
-                else {
-                  addToken(SLASH);
-                }
-                break;
-      case ' ' :
+      case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
+      case '/':
+        if (match('/')) {
+          while (peek() != '\n' && !isAtEnd())
+            advance(); // here advance will be returning each char it
+                       // encounters, but java will simply ignore them
+                       // since they aren't assingned to anything.
+        } else {
+          addToken(SLASH);
+        }
+        break;
+      case ' ':
       case '\t':
       case '\r':
-                break; // white spaces are not added. current will keep advancing 
-                        // as long as there is a white space.
+        break; // white spaces are not added. current will keep advancing
+               // as long as there is a white space.
       case '\n':
-                line++; break;
+        line++;
+        break;
 
       case '"':
-                string(); break;
+        string();
+        break;
 
-      default: 
-                if (isDigit(c)) {
-                  number();
-                } else{
-                  if (isAlpha(c)) {
-                  identifier();
-                  } else {
-                   Compiler.error(line, "Unexpected character.");
-                  }
-                }
+      default:
+        if (isDigit(c)) {
+          number();
+        } else {
+          if (isAlpha(c)) {
+            identifier();
+          } else {
+            Compiler.error(line, "Unexpected character.");
+          }
+        }
 
     }
   }
 
-  private boolean isAtEnd()
-  {
+  private boolean isAtEnd() {
     return current >= source.length();
   }
 
@@ -129,49 +146,55 @@ class Scanner {
     addToken(type, null);
   }
 
-  private void addToken(TokenType type, Object literal) { // this method is called  directly when the token is a literal.
-  // (numbers, strings..) so the literal/value gets stored correcly instead of null.
-    String text = source.substring(start, current); // passing curent instead of current -1 
-    // comes down to how java's String.substring(); works, it returns the substring from 
-    // the start index until the index right before endIndex so the last char (pointed by tha enbIndex arg)
+  private void addToken(TokenType type, Object literal) { // this method is called directly when the token is a literal.
+    // (numbers, strings..) so the literal/value gets stored correcly instead of
+    // null.
+    String text = source.substring(start, current); // passing curent instead of current -1
+    // comes down to how java's String.substring(); works, it returns the substring
+    // from
+    // the start index until the index right before endIndex so the last char
+    // (pointed by tha enbIndex arg)
     // is not included.
     tokens.add(new Token(type, text, literal, line));
   }
 
   private boolean match(char c) {
-    if (isAtEnd()) return false;
-    
-    if (source.charAt(current)  != c) return false;
+    if (isAtEnd())
+      return false;
+
+    if (source.charAt(current) != c)
+      return false;
 
     current++; // we only move to the next char if there is a match because current
-              // and the previous will be the same token.
-              // if there is no match the the previous will be tokened separately
-              // and current separately, not the same token.
+               // and the previous will be the same token.
+               // if there is no match the the previous will be tokened separately
+               // and current separately, not the same token.
     return true;
   }
 
-  private char peek()
-  {
-    if (isAtEnd()) return '\0';
+  private char peek() {
+    if (isAtEnd())
+      return '\0';
     return source.charAt(current);
   }
 
   private void string() {
-    while (!isAtEnd() && peek() != '"')
-    {
-      if (peek() == '\n') line++;
+    while (!isAtEnd() && peek() != '"') {
+      if (peek() == '\n')
+        line++;
       advance(); // or current++; which I think is faster.
     }
 
-    if  (isAtEnd())  {
-      Compiler.error(line,"Undetermined string.");
+    if (isAtEnd()) {
+      Compiler.error(line, "Undetermined string.");
     }
     advance(); // getting past the closing '"'.
 
     String text = source.substring(start + 1, current - 1); // to exclude the starting and ending '"'.
-    
+
     addToken(STRING_LITERAL, text);
   }
+
   private boolean isDigit(char c) {
     return (c >= '0' && c <= '9');
   }
@@ -179,46 +202,46 @@ class Scanner {
   private void number() {
     boolean isDouble = false;
     while (isDigit(peek())) {
-    advance();
+      advance();
     }
     if (peek() == '.' && isDigit(peekNext())) {
-       isDouble = true; // not an INT.
-       advance(); // eat the '.' 
-       while (isDigit(peek()))  advance();
-    } 
+      isDouble = true; // not an INT.
+      advance(); // eat the '.'
+      while (isDigit(peek()))
+        advance();
+    }
 
     if (isDouble) {
-      addToken(DOUBLE_LITERAL, Double.parseDouble(source.substring(start, current))); 
-    } 
-    else {  
+      addToken(DOUBLE_LITERAL, Double.parseDouble(source.substring(start, current)));
+    } else {
       addToken(INT_LITERAL, Integer.parseInt(source.substring(start, current)));
     }
   }
 
-    private char peekNext() {
-      if (current + 1 >= source.length()) return '\0';
+  private char peekNext() {
+    if (current + 1 >= source.length())
+      return '\0';
 
-      return source.charAt(current + 1);
-    }
+    return source.charAt(current + 1);
+  }
 
-    private void identifier() {
-      while (isAlphaNumeric(peek())) advance();
-      String text = source.substring(start, current);
-      TokenType type = keywords.get(text);
-      if (type == null) type = IDENTIFIER;
+  private void identifier() {
+    while (isAlphaNumeric(peek()))
+      advance();
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null)
+      type = IDENTIFIER;
 
-      addToken(type);
-    }
+    addToken(type);
+  }
 
-    private boolean isAlpha(char c) {
-      return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_');
-    }
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_');
+  }
 
-    private boolean isAlphaNumeric(char c) {
-      return isAlpha(c) || isDigit(c);
-    }
-
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
 
 }
-
-
